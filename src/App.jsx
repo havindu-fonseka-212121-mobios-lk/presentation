5bd5b4b9a4959091cc5a514f67bc38665726901e
havindu-fonseka-212121-mobios-lk <havindu.fonseka@mobios.lk>
2026-01-05 23:26:14 +0530

    feat: Update Home component import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Login from './pages/Login';
-import Home from './pages/HomeV2';
+import Home from './pages/Home';
 
 function App() {
   const { isAuthenticated } = useSelector((state) => state.auth);
